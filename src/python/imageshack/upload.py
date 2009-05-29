c924c609ac8597001e2cccd6d9056e265aa19d04
Vadim Zaliva <lord@crocodile.org>
2009-05-29 11:48:35 -0700

    import old code

---
+++ src/python/imageshack/upload.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+'''
+Upload image file to imageshack and returns XML document
+(simple version for image/PDF files, no video support)
+'''
+
+import urllib2_file
+import urllib2
+
+API_URL = 'http://www.imageshack.us/upload_api.php'
+
+def yesno(x):
+    if x:
+        return 'yes'
+    else:
+        return 'no'
+
+def upload_file(developer_key,
+                filename,
+                optsize=None,
+                user_cookie=None,
+                remove_bar=True,
+                tags=None,
+                public=True,
+                ):
+
+    data = {'fileupload' : open(filename,'rb'),
+            'key' : developer_key,
+            'rembar' : yesno(remove_bar),
+            'public' : yesno(public)
+            }
+
+    # Some optional parameters
+    if optsize:
+        data['optsize'] = optsize
+    if user_cookie:
+        data['cookie'] = user_cookie
+    if tags:
+        data['tags'] = tags
+    
+    req = urllib2.Request(API_URL, data, {})
+    u = urllib2.urlopen(req)
+    return u.read()
+    
+
+
+if __name__ == "__main__":
+    import sys
+    # Sample usage: upload.py <DEVELOPER KEY> <filename>
+    print upload_file(sys.argv[1], sys.argv[2])
+    
+    
+    
